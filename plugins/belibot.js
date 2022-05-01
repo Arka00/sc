f06be2462b91e6d72e7c343a3bf08a252cf4b443
Nanzz681 <adnanputra681@gmail.com>
2022-05-01 23:27:08 +0700

    awokawok

---
+++ plugins/belibot.js
@@ -0,0 +1,14 @@
+let fetch = require('node-fetch')
+let handler = async (m, { conn }) => {
+	conn.send2ButtonLoc(m.chat, await (await fetch(fla + 'sewa bot')).buffer(), `┌〔 List Berlangganan 〕
+├ Permanen
+├ 2 Bulan
+├ 1 Bulan
+├ Trial 1 jam
+└────
+`.trim(), watermark, 'Gopay', '#viagopay', 'Pulsa', '#viapulsa', m)
+}
+
+handler.command = /^sewabot$/i
+
+module.exports = handler
