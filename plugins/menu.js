02fbbaa8d30a27c820c0d8cd2262317a23aff94e
Nanzz <adnanputra681@gmail.com>
2022-05-02 00:10:54 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -24,7 +24,7 @@ const defaultMenu = {
 └───────⭓
 %readmore`.trimStart(),
   header: '┌───「%category 」───⭓\n│',
-  body: '│▸⭔* %cmd %islimit %isPremium',
+  body: '│▸⭔ %cmd %islimit %isPremium',
   footer: '└────⭓\n',
   after: `
 *%npmname@^%version*
